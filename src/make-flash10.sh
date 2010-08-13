bb757e2ee635acadc2c0d452cc7e05170122b248
Karl Varga <kjvarga@gmail.com>
2010-08-13 14:20:37 -0700

    Optimize the SWF builds

--- src/make-flash10.sh
+++ src/make-flash10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/Applications/flexsdk/bin/mxmlc -target-player=10.0.0 -use-network=false -o ../swf/soundmanager2_flash10.swf -file-specs "SoundManager2_AS3.as"
+/Applications/flexsdk/bin/mxmlc -optimize=true -target-player=10.0.0 -use-network=false -o ../swf/soundmanager2_flash10.swf -file-specs "SoundManager2_AS3.as"
 sed -e '/^$/d' -e '/DO_NOT_DELETE/b' -e '/flashDebugEnabled/b' -e '/writeDebug/d' -e '/flashDebug/d' SoundManager2_AS3.as > /tmp/SoundManager2_AS3.as
 sed -e '/^$/d' -e '/DO_NOT_DELETE/b' -e '/flashDebugEnabled/b' -e '/writeDebug/d' -e '/flashDebug/d' SoundManager2_SMSound_AS3.as > /tmp/SoundManager2_SMSound_AS3.as
-/Applications/flexsdk/bin/mxmlc -target-player=10.0.0 -use-network=false -o ../swf/soundmanager2_flash10_nodebug.swf -file-specs "/tmp/SoundManager2_AS3.as"
+/Applications/flexsdk/bin/mxmlc -optimize=true -target-player=10.0.0 -use-network=false -o ../swf/soundmanager2_flash10_nodebug.swf -file-specs "/tmp/SoundManager2_AS3.as"
--- src/make-flash9-debug.sh
+++ src/make-flash9-debug.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Applications/flexsdk/bin/mxmlc -debug=true -use-network=false -o ../swf/soundmanager2_flash9.swf -file-specs "SoundManager2_AS3.as"
+/Applications/flexsdk/bin/mxmlc -optimize=true -debug=true -use-network=false -o ../swf/soundmanager2_flash9.swf -file-specs "SoundManager2_AS3.as"
