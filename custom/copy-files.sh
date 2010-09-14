648bc671cec37d3dc93ce1d03516b9f833b63aef
Karl Varga <kjvarga@gmail.com>
2010-09-14 14:32:04 -0700

    Include the xdomain SWFs when we link/copy the files

--- custom/copy-files.sh
+++ custom/copy-files.sh
@@ -1,5 +1,8 @@
 rm ~/projects/musicsocial/public/soundmanager/*.swf
+rm -r ~/projects/musicsocial/public/soundmanager/soundmanager2_flash_xdomain
+mkdir -p ~/projects/musicsocial/public/soundmanager/soundmanager2_flash_xdomain
 rm ~/projects/musicsocial/public/javascripts/player/soundmanager2.js
 cp -f ~/projects/SoundManager2/swf/soundmanager2_flash*.swf ~/projects/musicsocial/public/soundmanager/
 cp -f ~/projects/SoundManager2/script/soundmanager2.js ~/projects/musicsocial/public/javascripts/player/
 cp -f ~/projects/SoundManager2/script/soundmanager2-nodebug.js ~/projects/musicsocial/public/javascripts/player/
+cp -f ~/projects/SoundManager2/swf/soundmanager2_flash_xdomain/soundmanager2_flash*.swf ~/projects/musicsocial/public/soundmanager/soundmanager2_flash_xdomain/
--- custom/link-files.sh
+++ custom/link-files.sh
@@ -1,5 +1,7 @@
 rm ~/projects/musicsocial/public/soundmanager/*.swf
+rm -r ~/projects/musicsocial/public/soundmanager/soundmanager2_flash_xdomain
 ln -s ~/projects/SoundManager2/swf/soundmanager2_flash9.swf ~/projects/musicsocial/public/soundmanager/soundmanager2_flash9.swf
 ln -s ~/projects/SoundManager2/swf/soundmanager2_flash9_debug.swf ~/projects/musicsocial/public/soundmanager/soundmanager2_flash9_debug.swf
 rm ~/projects/musicsocial/public/javascripts/player/soundmanager2.js
 ln -s ~/projects/SoundManager2/script/soundmanager2.js ~/projects/musicsocial/public/javascripts/player/soundmanager2.js
+ln -s ~/projects/SoundManager2/swf/soundmanager2_flash_xdomain/ ~/projects/musicsocial/public/soundmanager/soundmanager2_flash_xdomain
