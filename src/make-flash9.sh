219c04aa9dda31575450cdc420c90cdc6f16f658
Scott Schiller <sm2@schillmania.com>
2010-09-30 18:57:13 -0700

    static ish

--- src/make-flash9.sh
+++ src/make-flash9.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Applications/flexsdk/bin/mxmlc -use-network=false -optimize=true -o ../swf/soundmanager2_flash9_debug.swf -file-specs "SoundManager2_AS3.as"
+/Applications/flexsdk/bin/mxmlc -use-network=false -optimize=true -static-link-runtime-shared-libraries=true -o ../swf/soundmanager2_flash9_debug.swf -file-specs "SoundManager2_AS3.as"
