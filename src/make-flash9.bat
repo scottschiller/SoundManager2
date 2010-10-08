d22cf3856f0bd0ade827caf5985888efa5335506
Scott Schiller <sm2@schillmania.com>
2010-10-08 15:44:54 -0700

    add -debug

--- src/make-flash9.bat
+++ src/make-flash9.bat
@@ -1,4 +1,4 @@
 @REM this builds the soundmanager 2 SWF from source
 @REM using mxmlc from the Adobe open-source Flex SDK
 
-c:\progra~1\flexsdk\bin\mxmlc -use-network=false -optimize=true -o ../swf/soundmanager2_flash9_debug.swf -file-specs SoundManager2_AS3.as
+c:\progra~1\flexsdk\bin\mxmlc -debug=true -use-network=false -static-link-runtime-shared-libraries=true -optimize=true -o ../swf/soundmanager2_flash9_debug.swf -file-specs SoundManager2_AS3.as
