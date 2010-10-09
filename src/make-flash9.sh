5334540fee9202f4b592ab25dc2a4b09be78c4b4
Scott Schiller <sm2@schillmania.com>
2010-10-08 17:22:34 -0700

    Better-er build

--- src/make-flash9.sh
+++ src/make-flash9.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Applications/flexsdk/bin/mxmlc -debug=true -use-network=false -optimize=true -static-link-runtime-shared-libraries=true -o ../swf/soundmanager2_flash9_debug.swf -file-specs "SoundManager2_AS3.as"
+/Applications/flexsdk/bin/mxmlc -debug=true -static-link-runtime-shared-libraries=true -optimize=true -o ../swf/soundmanager2_flash9_debug.swf -file-specs SoundManager2_AS3.as
