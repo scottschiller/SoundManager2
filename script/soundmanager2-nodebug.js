510b5ce60a99fc7bc22aa7dfbb05f761b9f1410a
Scott Schiller <spam@schillmania.com>
2011-10-30 16:15:22 -0700

    V2.97a.20111030

--- script/soundmanager2-nodebug.js
+++ script/soundmanager2-nodebug.js
@@ -8,7 +8,7 @@
  * Code provided under the BSD License:
  * http://schillmania.com/projects/soundmanager2/license.txt
  *
- * V2.97a.20110918+DEV
+ * V2.97a.20111030
  */
 
 /*global window, SM2_DEFER, sm2Debugger, console, document, navigator, setTimeout, setInterval, clearInterval, Audio */
@@ -105,7 +105,7 @@ function SoundManager(smURL, smID) {
   };
   this.debugID = 'soundmanager-debug';
   this.debugURLParam = /([#?&])debug=1/i;
-  this.versionNumber = 'V2.97a.20110918+DEV';
+  this.versionNumber = 'V2.97a.20111030';
   this.version = null;
   this.movieURL = null;
   this.url = (smURL || null);
