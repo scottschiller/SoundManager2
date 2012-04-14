c3ab36162c6e3b0d58b9625c3415e52755504e89
Scott Schiller <spam@schillmania.com>
2012-04-13 18:32:01 -0700

    Correct HTML5 load under desktops etc., after Android fix.

--- script/soundmanager2-nodebug.js
+++ script/soundmanager2-nodebug.js
@@ -141,7 +141,7 @@ function SoundManager(smURL, smID) {
   _s = this, _flash = null, _sm = 'soundManager', _smc = _sm+'::', _h5 = 'HTML5::', _id, _ua = navigator.userAgent, _win = window, _wl = _win.location.href.toString(), _doc = document, _doNothing, _init, _fV, _on_queue = [], _debugOpen = true, _debugTS, _didAppend = false, _appendSuccess = false, _didInit = false, _disabled = false, _windowLoaded = false, _wDS, _wdCount = 0, _initComplete, _mixin, _addOnEvent, _processOnEvents, _initUserOnload, _delayWaitForEI, _waitForEI, _setVersionInfo, _handleFocus, _strings, _initMovie, _domContentLoaded, _winOnLoad, _didDCLoaded, _getDocument, _createMovie, _catchError, _setPolling, _initDebug, _debugLevels = ['log', 'info', 'warn', 'error'], _defaultFlashVersion = 8, _disableObject, _failSafely, _normalizeMovieURL, _oRemoved = null, _oRemovedHTML = null, _str, _flashBlockHandler, _getSWFCSS, _swfCSS, _toggleDebug, _loopFix, _policyFix, _complain, _idCheck, _waitingForEI = false, _initPending = false, _startTimer, _stopTimer, _timerExecute, _h5TimerCount = 0, _h5IntervalTimer = null, _parseURL,
   _needsFlash = null, _featureCheck, _html5OK, _html5CanPlay, _html5Ext, _html5Unload, _domContentLoadedIE, _testHTML5, _event, _slice = Array.prototype.slice, _useGlobalHTML5Audio = false, _hasFlash, _detectFlash, _badSafariFix, _html5_events, _showSupport,
   _is_iDevice = _ua.match(/(ipad|iphone|ipod)/i), _is_firefox = _ua.match(/firefox/i), _isIE = _ua.match(/msie/i), _isWebkit = _ua.match(/webkit/i), _isSafari = (_ua.match(/safari/i) && !_ua.match(/chrome/i)), _isOpera = (_ua.match(/opera/i)),
-  _likesHTML5 = (_ua.match(/(mobile|pre\/|xoom)/i) || _is_iDevice),
+  _mobileHTML5 = (_ua.match(/(mobile|pre\/|xoom)/i) || _is_iDevice),
   _isBadSafari = (!_wl.match(/usehtml5audio/i) && !_wl.match(/sm2\-ignorebadua/i) && _isSafari && !_ua.match(/silk/i) && _ua.match(/OS X 10_6_([3-7])/i)),
   _hasConsole = (typeof console !== 'undefined' && typeof console.log !== 'undefined'), _isFocused = (typeof _doc.hasFocus !== 'undefined'?_doc.hasFocus():null), _tryInitOnFocus = (_isSafari && typeof _doc.hasFocus === 'undefined'), _okToDisable = !_tryInitOnFocus, _flashMIME = /(mp3|mp4|mpa)/i,
   _emptyURL = 'about:blank',
@@ -164,7 +164,7 @@ function SoundManager(smURL, smID) {
     'highPerf': 'high_performance',
     'flashDebug': 'flash_debug'
   };
-  if (_likesHTML5) {
+  if (_mobileHTML5) {
     _s.useHTML5Audio = true;
     _s.preferFlash = false;
     if (_is_iDevice) {
@@ -1176,6 +1176,9 @@ function SoundManager(smURL, smID) {
       } else {
         _a.autobuffer = false;
         _a.preload = 'none';
+        if (!_mobileHTML5) {
+          _t.load();
+        }
       }
       _a.loop = (_iO.loops > 1 ? 'loop' : '');
       return _a;
@@ -1925,7 +1928,7 @@ function SoundManager(smURL, smID) {
   _startTimer = function(oSound) {
     if (!oSound._hasTimer) {
       oSound._hasTimer = true;
-      if (!_likesHTML5 && _s.html5PollingInterval) {
+      if (!_mobileHTML5 && _s.html5PollingInterval) {
         if (_h5IntervalTimer === null && _h5TimerCount === 0) {
           _h5IntervalTimer = window.setInterval(_timerExecute, _s.html5PollingInterval);
         }
@@ -1936,7 +1939,7 @@ function SoundManager(smURL, smID) {
   _stopTimer = function(oSound) {
     if (oSound._hasTimer) {
       oSound._hasTimer = false;
-      if (!_likesHTML5 && _s.html5PollingInterval) {
+      if (!_mobileHTML5 && _s.html5PollingInterval) {
         _h5TimerCount--;
       }
     }
