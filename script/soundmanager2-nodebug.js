d2b7eabd77ea235725201ef81a7749133e7dbb68
Scott Schiller <spam@schillmania.com>
2012-05-13 16:48:40 -0700

    V2.97a.20120513 - Fixes for Android 2.3 playback, onPosition() in HTML5, Flash 9 double-play edge case. Minor new Flash 9-specific features.
        Bug fixes
    
            Android 2.3 playback fix (some sounds started only after 2+ play attempts due to Android not liking load() immediately followed by play()?)
    
            onPosition() fix for HTML5 playback, items were mistakenly being removed.
    
            Fix RTMP stream duration reporting via onmetadata() (wrong value was being returned, previously.)
    
            Fix for Flash 9 double-play edge case found with pause() + setPosition() calls.
    
            JSLINT validation per 04-15-2012 edition rules, mostly removal of multiple return statements within if ... else blocks and inverted loop tricks.
    
        API updates
    
            Enabled waveform/eq/spectrum data features for MovieStar (H.264/NetStream content.)
    
            Added oncaptiondata() callback for caption data from Flash 9 (NetStream/RTMP-only, similar to onmetadata(). Contributor: GitHub user karma.)
    
            Added SMSound.onplayerror(), presently for Flash 9 (non-MovieStar/NetStream) which can detect lack of available sound hardware or 32-channel ceiling as reasons for playback failure.
    
            Added oncaptiondata() callback for caption data from Flash 9 (NetStream/RTMP-only, similar to onmetadata().)
    
        Miscellaneous
    
            Scaling added to 360 UI (via GitHub user tomasdev)

--- script/soundmanager2-nodebug.js
+++ script/soundmanager2-nodebug.js
@@ -8,7 +8,7 @@
  * Code provided under the BSD License:
  * http://schillmania.com/projects/soundmanager2/license.txt
  *
- * V2.97a.20120318+DEV
+ * V2.97a.20120513
  */
 
 /*global window, SM2_DEFER, sm2Debugger, console, document, navigator, setTimeout, setInterval, clearInterval, Audio */
@@ -98,7 +98,7 @@ function SoundManager(smURL, smID) {
   this.id = (smID || 'sm2movie');
   this.debugID = 'soundmanager-debug';
   this.debugURLParam = /([#?&])debug=1/i;
-  this.versionNumber = 'V2.97a.20120318+DEV';
+  this.versionNumber = 'V2.97a.20120513';
   this.version = null;
   this.movieURL = null;
   this.url = (smURL || null);
--- src/SoundManager2.as
+++ src/SoundManager2.as
@@ -36,7 +36,7 @@ class SoundManager2 {
 
   function SoundManager2() {
 
-    var version = "V2.97a.20120318+DEV";
+    var version = "V2.97a.20120513";
     var version_as = "(AS2/Flash 8)";
 
     /**
