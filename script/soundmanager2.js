a7740a200bbe6de80b0d8097ead1c63571de9c22
Scott Schiller <spam@schillmania.com>
2012-10-28 13:29:59 -0700

    V2.97a.20121018

--- script/soundmanager2.js
+++ script/soundmanager2.js
@@ -8,7 +8,7 @@
  * Code provided under the BSD License:
  * http://schillmania.com/projects/soundmanager2/license.txt
  *
- * V2.97a.20120916+DEV
+ * V2.97a.20121028
  */
 
 /*global window, SM2_DEFER, sm2Debugger, console, document, navigator, setTimeout, setInterval, clearInterval, Audio, opera */
@@ -181,7 +181,7 @@ function SoundManager(smURL, smID) {
 
   // dynamic attributes
 
-  this.versionNumber = 'V2.97a.20120916+DEV';
+  this.versionNumber = 'V2.97a.20121028';
   this.version = null;
   this.movieURL = null;
   this.altURL = null;
