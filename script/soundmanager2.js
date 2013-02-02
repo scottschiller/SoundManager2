cca3195f26caf55f0e5b10436ca2e8cc680f945b
Scott Schiller <spam@schillmania.com>
2013-02-02 09:15:00 -0800

    Improved load() on iOS (singleton HTML5 audio case), where "last URL" check was incorrect - https://getsatisfaction.com/schillmania/topics/autoload_on_ipad_loop_error
    Former-commit-id: 7907f09bb03b3aac331ddcb177f83b60e9199d92

--- script/soundmanager2.js
+++ script/soundmanager2.js
@@ -2711,9 +2711,9 @@ function SoundManager(smURL, smID) {
 
     this._setup_html5 = function(oOptions) {
 
-      var instanceOptions = mixin(s._iO, oOptions), d = decodeURI,
-          a = useGlobalHTML5Audio ? globalHTML5Audio  : s._a,
-          dURL = d(instanceOptions.url),
+      var instanceOptions = mixin(s._iO, oOptions),
+          a = useGlobalHTML5Audio ? globalHTML5Audio : s._a,
+          dURL = decodeURI(instanceOptions.url),
           sameURL;
 
       /**
@@ -2724,12 +2724,12 @@ function SoundManager(smURL, smID) {
 
       if (useGlobalHTML5Audio) {
 
-        if (dURL === lastGlobalHTML5URL) {
+        if (dURL === decodeURI(lastGlobalHTML5URL)) {
           // global HTML5 audio: re-use of URL
           sameURL = true;
         }
 
-      } else if (dURL === lastURL) {
+      } else if (dURL === decodeURI(lastURL)) {
 
         // options URL is the same as the "last" URL, and we used (loaded) it
         sameURL = true;
