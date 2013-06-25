3038e50a87747d47f80ff6989b463d77414abb06
Scott Schiller <spam@schillmania.com>
2013-06-24 19:57:41 -0700

    ontimeout: function(status) {} callback parameter corrections. Fix missing error { type: 'INIT_TIMEOUT' } for missing/404 SWF case, and INIT_FLASHBLOCK for flashblock-specific case.

--- script/soundmanager2-nodebug.js
+++ script/soundmanager2-nodebug.js
@@ -107,7 +107,7 @@ function SoundManager(smURL, smID) {
   this.id = (smID || 'sm2movie');
   this.debugID = 'soundmanager-debug';
   this.debugURLParam = /([#?&])debug=1/i;
-  this.versionNumber = 'V2.97a.20130512';
+  this.versionNumber = 'V2.97a.20130512+DEV';
   this.version = null;
   this.movieURL = null;
   this.altURL = null;
@@ -2458,7 +2458,7 @@ function SoundManager(smURL, smID) {
             if (!sm2.useFlashBlock && canIgnoreFlash) {
               rebootIntoHTML5();
             } else {
-              processOnEvents({type:'ontimeout', ignoreInit: true});
+              processOnEvents({type:'ontimeout', ignoreInit: true, error: {type: 'INIT_FLASHBLOCK'}});
             }
           }
         } else {
@@ -2507,10 +2507,8 @@ function SoundManager(smURL, smID) {
         error;
     if (!wasTimeout) {
       didInit = true;
-      if (disabled) {
-        error = {type: (!hasFlash && needsFlash ? 'NO_FLASH' : 'INIT_TIMEOUT')};
-      }
     }
+    error = {type: (!hasFlash && needsFlash ? 'NO_FLASH' : 'INIT_TIMEOUT')};
     if (disabled || bNoDisable) {
       if (sm2.useFlashBlock && sm2.oMC) {
         sm2.oMC.className = getSWFCSS() + ' ' + (sm2.getMoviePercent() === null?swfCSS.swfTimedout:swfCSS.swfError);
