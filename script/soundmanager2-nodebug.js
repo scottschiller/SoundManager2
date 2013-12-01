624217efe42287b6eee9be0cafe6cb7327a0a050
Scott Schiller <spam@schillmania.com>
2013-11-30 17:29:50 -0800

    V2.97a.20131201

--- script/soundmanager2-nodebug.js
+++ script/soundmanager2-nodebug.js
@@ -8,7 +8,7 @@
  * Code provided under the BSD License:
  * http://schillmania.com/projects/soundmanager2/license.txt
  *
- * V2.97a.20130512+DEV
+ * V2.97a.20131201
  */
 
 /*global window, SM2_DEFER, sm2Debugger, console, document, navigator, setTimeout, setInterval, clearInterval, Audio, opera */
@@ -107,7 +107,7 @@ function SoundManager(smURL, smID) {
   this.id = (smID || 'sm2movie');
   this.debugID = 'soundmanager-debug';
   this.debugURLParam = /([#?&])debug=1/i;
-  this.versionNumber = 'V2.97a.20130512+DEV';
+  this.versionNumber = 'V2.97a.20131201';
   this.version = null;
   this.movieURL = null;
   this.altURL = null;
--- script/soundmanager2.js
+++ script/soundmanager2.js
@@ -8,7 +8,7 @@
  * Code provided under the BSD License:
  * http://schillmania.com/projects/soundmanager2/license.txt
  *
- * V2.97a.20130512+DEV
+ * V2.97a.20131201
  */
 
 /*global window, SM2_DEFER, sm2Debugger, console, document, navigator, setTimeout, setInterval, clearInterval, Audio, opera */
@@ -189,7 +189,7 @@ function SoundManager(smURL, smID) {
 
   // dynamic attributes
 
-  this.versionNumber = 'V2.97a.20130512+DEV';
+  this.versionNumber = 'V2.97a.20131201';
   this.version = null;
   this.movieURL = null;
   this.altURL = null;
