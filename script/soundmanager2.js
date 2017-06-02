6f91ba26e180e4043fc188de17d07c24cc8c28c5
Scott Schiller <spam@schillmania.com>
2017-06-01 19:45:31 -0700

    V2.97a.20170601!

--- script/soundmanager2.js
+++ script/soundmanager2.js
@@ -8,7 +8,7 @@
  * Code provided under the BSD License:
  * http://schillmania.com/projects/soundmanager2/license.txt
  *
- * V2.97a.20150601+DEV
+ * V2.97a.20170601
  */
 
 /**
@@ -206,7 +206,7 @@ function SoundManager(smURL, smID) {
 
   // dynamic attributes
 
-  this.versionNumber = 'V2.97a.20150601+DEV';
+  this.versionNumber = 'V2.97a.20170601';
   this.version = null;
   this.movieURL = null;
   this.altURL = null;
